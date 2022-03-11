c169e5d5d1715051908c8c1948d17c9fe0cefdb9
ChucklesDroid <mj.akarsh@gmail.com>
2022-03-11 17:24:39 +0530

    added videoframe data structure

---
+++ Camera/videoframes.h
@@ -0,0 +1,6 @@
+// Data Structure representing batch of video frames
+
+typedef struct Videoframes{
+	char location[40] ; 										// location of frames batch
+	int deviceId ; 						 							// Terminating no of its address
+} videoframes;
